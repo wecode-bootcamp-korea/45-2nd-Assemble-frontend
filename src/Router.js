a1e2bcea520e81c7af82c3d7d08899acd22d5820
Patata <patattta93@gmail.com>
2023-05-19 18:57:56 +0900

    Modify: 초기세팅 수정

--- src/Router.js
+++ src/Router.js
@@ -6,6 +6,7 @@ import Mypage from "./pages/Mypage/Mypage";
 import Header from "./components/Header/Header";
 import Nav from "./components/Nav/Nav";
 import Footer from "./components/Footer/Footer";
+import ProductDetails from "./pages/ProductDetails/ProductDetails";
 
 const Router = () => {
   return (
@@ -16,6 +17,7 @@ const Router = () => {
         <Route path="/" element={<Main />} />
         <Route path="/matching" element={<Matching />} />
         <Route path="/mypage" element={<Mypage />} />
+        <Route path="/court" element={<ProductDetails />} />
       </Routes>
       <Footer />
     </BrowserRouter>
---
+++ src/pages/ProductDetails/ProductDetails.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ProductDetails = () => {
+  return <div>상세페이지</div>;
+};
+
+export default ProductDetails;
