a2415a18d663bceaee4722903c4f86a824b0476f
Patata <patattta93@gmail.com>
2023-05-19 18:29:18 +0900

    Add: Header,Nav,Footer

--- src/Router.js
+++ src/Router.js
@@ -3,15 +3,21 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Main from "./pages/Main/Main";
 import Matching from "./pages/Matching/Matching";
 import Mypage from "./pages/Mypage/Mypage";
+import Header from "./components/Header/Header";
+import Nav from "./components/Nav/Nav";
+import Footer from "./components/Footer/Footer";
 
 const Router = () => {
   return (
     <BrowserRouter>
+      <Header />
+      <Nav />
       <Routes>
         <Route path="/" element={<Main />} />
         <Route path="/matching" element={<Matching />} />
         <Route path="/mypage" element={<Mypage />} />
       </Routes>
+      <Footer />
     </BrowserRouter>
   );
 };
---
+++ src/components/Nav/Nav.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Nav = () => {
+  return <div>네비게이션</div>;
+};
+
+export default Nav;
