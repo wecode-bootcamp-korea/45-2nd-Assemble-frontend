5356da77b8afaa86fe47c6bbfbf4570e299e417d
jeonseungbeom <7237sbj@gmail.com>
2023-05-23 15:03:45 +0900

    Add: reservationStatusPage 레이아웃

--- src/Router.js
+++ src/Router.js
@@ -1,12 +1,12 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Main from "./pages/Main/Main";
-import Matching from "./pages/Matching/Matching";
-import Mypage from "./pages/Mypage/Mypage";
 import Header from "./components/Header/Header";
 import Nav from "./components/Nav/Nav";
 import Footer from "./components/Footer/Footer";
+import Main from "./pages/Main/Main";
 import ProductDetails from "./pages/ProductDetails/ProductDetails";
+import Matching from "./pages/Matching/Matching";
+import ReservationStatusPage from "./pages/ReservationStatusPage/ReservationStatusPage";
 
 const Router = () => {
   return (
@@ -15,9 +15,12 @@ const Router = () => {
       <Nav />
       <Routes>
         <Route path="/" element={<Main />} />
-        <Route path="/matching" element={<Matching />} />
-        <Route path="/mypage" element={<Mypage />} />
         <Route path="/court" element={<ProductDetails />} />
+        <Route path="/matching" element={<Matching />} />
+        <Route
+          path="/reservationstatuspage"
+          element={<ReservationStatusPage />}
+        />
       </Routes>
       <Footer />
     </BrowserRouter>
