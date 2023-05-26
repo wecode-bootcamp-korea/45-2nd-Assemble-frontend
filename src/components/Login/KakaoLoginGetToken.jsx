5098096018009b4baa66f9190f23014c02bcc17a
min2oyo <min2u@outlook.com>
2023-05-26 21:58:09 +0900

    코드 정리

--- src/components/Login/KakaoLoginGetToken.jsx
+++ src/components/Login/KakaoLoginGetToken.jsx
@@ -7,15 +7,16 @@ const KakaoLoginGetToken = () => {
   let code = searchParams.get(`code`);
 
   useEffect(() => {
-    axios
-      .get(
-        `http://10.58.52.94:3000/users/kakaologin?code=${code}`
-      )
-      .then(res => {
-        localStorage.setItem('TOKEN', res.data.accessToken)
-        window.close();
-    })
-      .catch(err=>alert(err))
+    // axios
+    // .get(
+    //   `http://10.58.52.94:3000/users/kakaologin?code=${code}`
+    //   )
+    //   .then(res => {
+    //     // localStorage.setItem('TOKEN', res.data.accessToken)
+    //     window.close();
+    //   })
+    //   .catch(err=>alert(err))
+      localStorage.setItem('TOKEN', `임시 토큰`)
   }, [code]);
 
   return <div />;
